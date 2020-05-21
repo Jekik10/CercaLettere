7e8637f1ca8c70802a695171f41cc8e578c58d23
Jekik <49094209+Jekik10@users.noreply.github.com>
2020-05-21 10:45:18 +0200

    Add files via upload

---
+++ righi.samuele.5G.CercaLettere2/Alfabeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// write in 2019 by maurizio.conti@fablabromagna.org
+
+public static class Alfabeto
+{
+    public static bool Verifica(string input)
+    {
+        string alfabeto = "abcdefghilmnopqrstuvz";
+            foreach (char c in input.ToLower())
+            {
+                if (alfabeto.Contains(c))
+                {
+                    alfabeto = alfabeto.Replace(c.ToString(), string.Empty);
+                }
+            }
+
+            return alfabeto.Length == 0;
+    }
+}
